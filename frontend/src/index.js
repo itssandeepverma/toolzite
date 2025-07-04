6986441f722e3e12105484d216964f6a7f92ebe1
Sandeep <sandeep@192.168.1.9>
2025-07-04 13:10:52 +0530

    fix bugs blank screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
+// ① Import the CRA service-worker registration helper
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -13,3 +16,6 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// ② Tell it to unregister any existing SW so clients always load the fresh build:
+serviceWorkerRegistration.unregister();
